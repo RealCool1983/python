2b49b4bbba2f57f4899bd326b57aee449f072d37
rex.wu <rex.wu@m2k.com.tw>
2017-01-04 10:18:13 +0800

    [bat]add git_auto.bat

---
+++ git_Auto/git_sync.bat
@@ -0,0 +1,16 @@
+
+
+
+cd D:\3S_PC\sourceCode\SSD\MP_UI\source_code\GIT_MP_UI_BIN_GRADE\v1.0\v1.0.2016.918_Temp1
+
+git pull google master
+
+git push google master
+
+
+
+cd D:\3S_PC\sourceCode\SSD\MP_UI\source_code\GIT_MP_UI\v1.0\v1.0.2016.918_Temp1
+
+git pull google master
+
+git push google master
