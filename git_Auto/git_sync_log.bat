d376187fca74eb89e18d6e85030d150c75803b29
rex.wu <rex.wu@m2k.com.tw>
2017-02-10 16:18:05 +0800

    update

--- git_Auto/git_sync_log.bat
+++ git_Auto/git_sync_log.bat
@@ -4,21 +4,21 @@
 cd D:\3S_PC\sourceCode\SSD\MP_UI\VC6\BIN_GRADE_V1.0
 
 git pull ssd-bin-grade-log master
-
 git push ssd-bin-grade-log master
-
+git pull ssd-bin-grade-log master
 
 
 cd D:\3S_PC\sourceCode\SSD\MP_UI\VC6\GIT_MP_UI\V1.0
 
 git pull ssd-mp-ui-log master
-
 git push ssd-mp-ui-log master
+git pull ssd-mp-ui-log master
+
+cd D:\3S_PC\python\
 
 
-cd D:\3S_PC\python\3S_AUTO
-
-git push toolgit
+git pull origin master
+git push origin master
 git pull origin master
 
 pause
--- git_Auto/git_sync_release.bat
+++ git_Auto/git_sync_release.bat
@@ -2,23 +2,22 @@
 
 
 cd D:\3S_PC\sourceCode\SSD\MP_UI\VC6\BIN_GRADE_V1.0
-
 git pull ssd-bin-grade-release master
-
 git push ssd-bin-grade-release master
-
+git pull ssd-bin-grade-release master
 
 
 cd D:\3S_PC\sourceCode\SSD\MP_UI\VC6\GIT_MP_UI\V1.0
 
 git pull ssd-mp-ui-release master
-
 git push ssd-mp-ui-release master
+git pull ssd-mp-ui-release master
 
-
-cd D:\3S_PC\python\3S_AUTO
-
-git push toolgit
+cd D:\3S_PC\python\
 git pull origin master
+git push origin master
+git pull origin master
+
+
 
 pause
