b5d3cda2f95b529e69852a2bb803e2071fb4da80
rex.wu <rex.wu@m2k.com.tw>
2017-02-10 16:04:40 +0800

    Divided into log, release

--- git_Auto/git_sync_log.bat
+++ git_Auto/git_sync_log.bat
@@ -3,17 +3,17 @@
 
 cd D:\3S_PC\sourceCode\SSD\MP_UI\VC6\BIN_GRADE_V1.0
 
-git pull google master
+git pull ssd-bin-grade-log master
 
-git push google master
+git push ssd-bin-grade-log master
 
 
 
 cd D:\3S_PC\sourceCode\SSD\MP_UI\VC6\GIT_MP_UI\V1.0
 
-git pull google master
+git pull ssd-mp-ui-log master
 
-git push google master
+git push ssd-mp-ui-log master
 
 
 cd D:\3S_PC\python\3S_AUTO
