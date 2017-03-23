3607845d449ed8a5b02edb20fe718e04eb8a060c
rex.wu <rex.wu@m2k.com.tw>
2017-03-23 13:51:28 +0800

    rem ssd-bin-grade-log, add ssd-se-tools

--- git_Auto/git_sync_log.bat
+++ git_Auto/git_sync_log.bat
@@ -1,9 +1,13 @@
 
-cd D:\3S_PC\sourceCode\SSD\VC6\BIN_GRADE_V1.0
-git pull ssd-bin-grade-log master
-git push ssd-bin-grade-log master
-git pull ssd-bin-grade-log master
+REM cd D:\3S_PC\sourceCode\SSD\VC6\BIN_GRADE_V1.0
+REM git pull ssd-bin-grade-log master
+REM git push ssd-bin-grade-log master
+REM git pull ssd-bin-grade-log master
 
+cd D:\3S_PC\sourceCode\SSD\VC6\SE_Tools
+git pull ssd-se-tools master
+git push ssd-se-tools master
+git pull ssd-se-tools master
 
 cd D:\3S_PC\sourceCode\SSD\VC6\GIT_MP_UI\V1.0
 git pull ssd-mp-ui-log master
