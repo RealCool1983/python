90809f56b7effce6b4ae7933be2011a480dcd7cc
rex.wu <rex.wu@m2k.com.tw>
2017-06-15 15:48:22 +0800

    add delectExtraFile, vc6, vc2013

---
+++ 3S_AUTO/windows_bat/deleteExtraFile_VC2013.bat
@@ -0,0 +1,11 @@
+rmdir workHistory /S/Q
+rmdir releaseNote /S/Q
+rmdir .\src\Debug  /S/Q
+rmdir .\src\Release  /S/Q
+rmdir .\src\AES_Security\Debug  /S/Q
+rmdir \src\AES_Security\Release  /S/Q
+
+del .\*.filters /S/Q 
+del .\*.user /S/Q 
+del .\*.sdf /S/Q 
+del .\*.suo /S/Q/F/A
---
+++ 3S_AUTO/windows_bat/deleteExtraFile_VC6.bat
@@ -0,0 +1,18 @@
+rmdir workHistory /S/Q
+rmdir releaseNote /S/Q
+rmdir .\src\Debug  /S/Q
+rmdir .\src\Release  /S/Q
+rmdir .\src\AES_Security\Debug  /S/Q
+rmdir \src\AES_Security\Release  /S/Q
+
+del .\*.filters /S/Q 
+del .\*.user /S/Q 
+del .\*.sdf /S/Q 
+del .\*.suo /S/Q/F/A
+del .\*.ncb /S/Q/F/A
+
+del .\src\*.ncb /S/Q/F/A
+del .\src\*.opt /S/Q/F/A
+del .\src\*.plg /S/Q/F/A
+del .\src\*.clw /S/Q/F/A
+del .\src\*.APS /S/Q/F/A
