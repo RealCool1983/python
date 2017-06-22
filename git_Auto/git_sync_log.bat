43f9576de2311e5d6de8760ead6d123bff19e949
rex.wu <rex.wu@m2k.com.tw>
2017-06-22 14:45:40 +0800

    remove D:\3S_PC\sourceCode\SSD\VC6\GIT_MP

--- git_Auto/git_sync_log.bat
+++ git_Auto/git_sync_log.bat
@@ -5,7 +5,6 @@ REM git push ssd-bin-grade-log master
 REM git pull ssd-bin-grade-log master
 
 
-
 cd D:\3S_PC\sourceCode\SSD\VS2008
 git pull project-vs2010 master
 git push project-vs2010 master
@@ -20,13 +19,6 @@ cd D:\3S_PC\sourceCode\SSD\VC6\GIT_MP_UI\V1.0
 git pull ssd-mp-ui-log master
 git push ssd-mp-ui-log master
 git pull ssd-mp-ui-log master
-
-
-cd D:\3S_PC\sourceCode\SSD\VC6\GIT_MP
-git pull ssd-mp-log master
-git push ssd-mp-log master
-git pull ssd-mp-log master
-
 
 
 cd D:\3S_PC\python
