e330250dc90b0cb9ecf2ff3351282b7c87333cec
rex.wu <rex.wu@m2k.com.tw>
2017-07-20 17:38:10 +0800

    update sync log bat

--- git_Auto/git_sync_log.bat
+++ git_Auto/git_sync_log.bat
@@ -1,19 +1,13 @@
-
-REM cd D:\3S_PC\sourceCode\SSD\VC6\BIN_GRADE_V1.0
-REM git pull ssd-bin-grade-log master
-REM git push ssd-bin-grade-log master
-REM git pull ssd-bin-grade-log master
-
+cd D:\3S_PC\sourceCode\SSD\SSD_tools
+git pull ssd-se-tools master
+git push ssd-se-tools master
+git pull ssd-se-tools master
 
 cd D:\3S_PC\sourceCode\SSD\VS2008
 git pull project-vs2010 master
 git push project-vs2010 master
 git pull project-vs2010 master
 
-cd D:\3S_PC\sourceCode\SSD\VC6\SE_Tools
-git pull ssd-se-tools master
-git push ssd-se-tools master
-git pull ssd-se-tools master
 
 cd D:\3S_PC\sourceCode\SSD\VC6\GIT_MP_UI\V1.0
 git pull ssd-mp-ui-log master
@@ -33,3 +27,13 @@ git pull origin master
 
 
 pause
+
+REM cd D:\3S_PC\sourceCode\SSD\VC6\SE_Tools
+REM git pull ssd-se-tools master
+REM git push ssd-se-tools master
+REM git pull ssd-se-tools master
+
+REM cd D:\3S_PC\sourceCode\SSD\VC6\BIN_GRADE_V1.0
+REM git pull ssd-bin-grade-log master
+REM git push ssd-bin-grade-log master
+REM git pull ssd-bin-grade-log master
