9bbd56826d78be3ece8d7221d4c6de60a8fb0fea
rex.wu <rex.wu@m2k.com.tw>
2017-07-24 14:33:39 +0800

    update deleteExtraFile_VC2013

--- 3S_AUTO/windows_bat/deleteExtraFile_VC2013.bat
+++ 3S_AUTO/windows_bat/deleteExtraFile_VC2013.bat
@@ -3,7 +3,8 @@ rmdir releaseNote /S/Q
 rmdir .\src\Debug  /S/Q
 rmdir .\src\Release  /S/Q
 rmdir .\src\AES_Security\Debug  /S/Q
-rmdir \src\AES_Security\Release  /S/Q
+rmdir .\src\AES_Security\Release  /S/Q
+rmdir .\src\ipch  /S/Q
 
 del .\*.filters /S/Q 
 del .\*.user /S/Q 
