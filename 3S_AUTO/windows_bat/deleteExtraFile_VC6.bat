6660432b38d9ab686bcd2fe597acd9cecc4b948e
rex.wu <rex.wu@m2k.com.tw>
2017-08-10 18:03:18 +0800

    update deleteExtraFile_VC6

--- 3S_AUTO/windows_bat/deleteExtraFile_VC6.bat
+++ 3S_AUTO/windows_bat/deleteExtraFile_VC6.bat
@@ -6,12 +6,18 @@ rmdir .\src\Setting  /S/Q
 rmdir .\src\SortSetting  /S/Q
 rmdir .\src\AES_Security\Debug  /S/Q
 rmdir \src\AES_Security\Release  /S/Q
+rmdir .git  /S/Q
+rmdir MP_UI_DOC  /S/Q
+rmdir.\src\table  /S/Q
+
 
 del .\*.filters /S/Q 
 del .\*.user /S/Q 
 del .\*.sdf /S/Q 
 del .\*.suo /S/Q/F/A
 del .\*.ncb /S/Q/F/A
+del *.gitignore /S/Q/F/A
+
 
 del .\src\*.ncb /S/Q/F/A
 del .\src\*.opt /S/Q/F/A
