d1f1123caceb23f11481c99769d0897edf98c3aa
rex.wu <rex.wu@m2k.com.tw>
2017-09-13 14:16:15 +0800

    REM git pull project-vs2010 master, add git pull project-vs2008 master

--- git_Auto/git_sync_log.bat
+++ git_Auto/git_sync_log.bat
@@ -4,9 +4,9 @@ git push ssd-se-tools master
 git pull ssd-se-tools master
 
 cd D:\3S_PC\sourceCode\SSD\VS2008
-git pull project-vs2010 master
-git push project-vs2010 master
-git pull project-vs2010 master
+git pull project-vs2008 master
+git push project-vs2008 master
+git pull project-vs2008 master
 
 
 cd D:\3S_PC\sourceCode\SSD\VC6\GIT_MP_UI\V1.0
@@ -44,3 +44,8 @@ REM cd D:\3S_PC\sourceCode\SSD\VC6\BIN_GRADE_V1.0
 REM git pull ssd-bin-grade-log master
 REM git push ssd-bin-grade-log master
 REM git pull ssd-bin-grade-log master
+
+REM cd D:\3S_PC\sourceCode\SSD\VS2008
+REM git pull project-vs2010 master
+REM git push project-vs2010 master
+REM git pull project-vs2010 master
