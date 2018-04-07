d3cfc340c072c75e284f4db56317f787ed3425fb
Rex.Wu <Rex.Wu@m2k.com.tw>
2018-04-07 10:53:00 +0800

    add git sync python

---
+++ git_Auto/git_sync_python.bat
@@ -0,0 +1,24 @@
+
+cd D:\REF\python
+git pull origin master
+git push origin master
+git pull origin master
+
+
+
+pause
+
+REM cd D:\3S_PC\sourceCode\SSD\VC6\SE_Tools
+REM git pull ssd-se-tools master
+REM git push ssd-se-tools master
+REM git pull ssd-se-tools master
+
+REM cd D:\3S_PC\sourceCode\SSD\VC6\BIN_GRADE_V1.0
+REM git pull ssd-bin-grade-log master
+REM git push ssd-bin-grade-log master
+REM git pull ssd-bin-grade-log master
+
+REM cd D:\3S_PC\sourceCode\SSD\VS2008
+REM git pull project-vs2010 master
+REM git push project-vs2010 master
+REM git pull project-vs2010 master
